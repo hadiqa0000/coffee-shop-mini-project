f603bec935417f480a49bedecf9a41afa5233847
Hadiqa <hadiqaimrannn0@gmail.com>
2026-07-16 13:46:37 +0300

    forgot to save file while committing

--- ddl.sql
+++ ddl.sql
@@ -4,6 +4,7 @@ CREATE TABLE CoffeeShop(
     shop_address VARCHAR(255) NOT NULL,
     shop_phone VARCHAR(11) NOT NULL,
     shop_opened_at DATE NOT NULL,
+    operating_hours JSONB NOT NULL,
     PRIMARY KEY(shop_id)
 );
 
@@ -38,7 +39,7 @@ CREATE TABLE Orders(
     shop_id BIGINT NOT NULL,
     order_id BIGINT GENERATED ALWAYS AS IDENTITY,
     employee_id BIGINT NOT NULL,
-    ordered_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    ordered_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, --timestamp, which means date and time 
     order_status VARCHAR(20) NOT NULL DEFAULT 'pending' CHECK (order_status IN('pending', 'served', 'cancelled')),
     order_subtotal DECIMAL(12,2) NOT NULL,
     order_tax DECIMAL(15,2) NOT NULL,
@@ -66,7 +67,7 @@ CREATE TABLE Payment(
     payment_id BIGINT GENERATED ALWAYS AS IDENTITY,
     shop_id BIGINT NOT NULL,
     order_id BIGINT NOT NULL,
-    paid_at TIMESTAMP NULL, --cannot be before shop_opened_at
+    paid_at TIMESTAMP NULL, --cannot be before shop_opened_at, it is a timestamp, which means date AND time
     payment_method VARCHAR(20) CHECK (payment_method IN ('cash', 'card')),
     payment_status VARCHAR(20) DEFAULT 'completed' CHECK(payment_status IN('pending', 'completed', 'cancelled')),
     amount DECIMAL(12,2) NOT NULL, --should be equal to order_total
