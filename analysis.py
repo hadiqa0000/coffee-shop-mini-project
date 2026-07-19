f4fe2f3f139c5a7fee6b6c915da8d69f8a81004a
Hadiqa <hadiqaimrannn0@gmail.com>
2026-07-19 20:53:22 +0300

    checking for missing values in any columns in Employee

--- analysis.py
+++ analysis.py
@@ -7,14 +7,15 @@ import seaborn as sns
 DATABASE_URL = f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@localhost:5432/postgres"
 
 engine = create_engine(DATABASE_URL)
- dfshops = pd.read_sql_query("SELECT * FROM CoffeeShop;", engine)
- dfproducts = pd.read_sql_query("SELECT * FROM Products;", engine)
- dfemployee = pd.read_sql_query("SELECT * FROM Employee;", egine)
- dforders = pd.read_sql_query("SELECT * FROM Orders", engine)
- dforderitem = pd.read_sql_query("SELECT * FROM OrderItem", engine)
- dfpayment = pd.read_sql_query("SELECT * FROM Payment", engine)
+dfshops = pd.read_sql_query("SELECT * FROM CoffeeShop;", engine)
+dfproducts = pd.read_sql_query("SELECT * FROM Products;", engine)
+dfemployee = pd.read_sql_query("SELECT * FROM Employee;", egine)
+dforders = pd.read_sql_query("SELECT * FROM Orders", engine)
+dforderitem = pd.read_sql_query("SELECT * FROM OrderItem", engine)
+dfpayment = pd.read_sql_query("SELECT * FROM Payment", engine)
  
  
- dforders['ordered_at']= pd.to_datetime(dforders['ordered_at']
- dfemployee['employee_dob']= pd.to_datetime(dforders['employee_dob']
- dfemployee['employee_hire_date']= pd.to_datetime(dforders['employee_hire_date']
+dforders['ordered_at']= pd.to_datetime(dforders['ordered_at']
+dfemployee['employee_dob']= pd.to_datetime(dforders['employee_dob']
+dfemployee['employee_hire_date']= pd.to_datetime(dforders['employee_hire_date']
+print(df_employees.isnull().sum()[df_employees.isnull().sum() > 0])
