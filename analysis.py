bbc1ab86ff7082d9b12aa13368f891e491045187
Hadiqa <hadiqaimrannn0@gmail.com>
2026-07-19 20:46:31 +0300

    breaking up all the tables in my database and loading them as data frames using pandas to analyse later

--- analysis.py
+++ analysis.py
@@ -7,5 +7,9 @@ import seaborn as sns
 DATABASE_URL = f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@localhost:5432/postgres"
 
 engine = create_engine(DATABASE_URL)
- shops = pd.read_sql_query("SELECT * FROM CoffeeShop;", engine)
- products = pd.read_sql_query("SELECT * FROM Products;", engine)
+ dfshops = pd.read_sql_query("SELECT * FROM CoffeeShop;", engine)
+ dfproducts = pd.read_sql_query("SELECT * FROM Products;", engine)
+ dfemployees = pd.read_sql_query("SELECT * FROM Emolployee;", egine)
+ dforders = pd.read_sql_query("SELECT * FROM Orders", engine)
+ dforderitem = pd.read_sql_query("SELECT * FROM OrderItem", engine)
+ dfpayment = pd.read_sql_query("SELECT * FROM Payment", engine)
